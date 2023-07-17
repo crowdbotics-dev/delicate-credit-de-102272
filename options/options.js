d75f37b46433446bad48b47e67ec91db17f50bb2
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 14:28:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-credit-de-102272",
+  url: "https://delicate-credit-de-102272.botics.co",
+  api: "https://delicate-credit-de-102272.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
